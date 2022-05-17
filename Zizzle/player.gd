9d7e34787bae82c1d28e587f1ae03319f4449c71
Devin Leamy <devinleamy@gmail.com>
2022-05-17 14:09:12 -0400

    converted player into RigidBody2D and added basic jumping

--- Zizzle/player.gd
+++ Zizzle/player.gd
@@ -1,9 +1,8 @@
 extends RigidBody2D
 
-
-var velocity = Vector2()
-var horiz_accel = 100
-var max_horiz_vel = 500
+export var horiz_accel = 100
+export var max_horiz_vel = 500
+export var jump_power = 300
 
 var screen_size
 export var start_position = Vector2.ZERO
@@ -12,25 +11,32 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	position = start_position
 
-func get_movement_inputs():
+func _integrate_forces(state):
+	var velocity = state.get_linear_velocity()
+	var step = state.get_step()
+	
+	var left = Input.is_action_pressed("left")
+	var right = Input.is_action_pressed("right")
+	var jump = Input.is_action_pressed("jump")
 	
 	# Accelerate to max velocity if direction is held
-	if Input.is_action_pressed("left"):
+	if left:
 		velocity.x = max(velocity.x - horiz_accel, -max_horiz_vel)
-	elif Input.is_action_pressed("right"):
+	elif right:
 		velocity.x = min(velocity.x + horiz_accel, max_horiz_vel)
-	
-	# Decelerate when not moving
 	else:
+		# Decelerate when not moving
 		if velocity.x < 0.0:
 			velocity.x = max(velocity.x + horiz_accel, 0.0)
 		elif velocity.x > 0.0:
 			velocity.x = min(velocity.x - horiz_accel, 0.0)
-
+	
+	if jump:
+		velocity.y = -jump_power
+	
+	velocity += state.get_total_gravity() * step
+	state.set_linear_velocity(velocity)
+	
 func _physics_process(delta):
-	get_movement_inputs()
-	
-	position = position + velocity * delta
-	
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
