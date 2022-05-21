3c0bcef8bdb4d3803b168993817162648849da5b
winterbeak <winterbeak@gmail.com>
2022-05-21 13:49:34 -0400

    Edit comments and whitespace

--- Zizzle/src/Player/player.gd
+++ Zizzle/src/Player/player.gd
@@ -24,25 +24,30 @@ func get_inputs(delta):
 		velocity.x = max(velocity.x - horiz_accel, -max_horiz_vel)
 	elif right:
 		velocity.x = min(velocity.x + horiz_accel, max_horiz_vel)
+		
+	# Decelerate when not moving
 	else:
-		# Decelerate when not moving
 		if velocity.x < 0.0:
 			velocity.x = max(velocity.x + horiz_accel, 0.0)
 		elif velocity.x > 0.0:
 			velocity.x = min(velocity.x - horiz_accel, 0.0)
-	
+		
 	if is_on_floor():
 		velocity.y = 0.0
 		coyote_time_timer = 0.0
+		
 	if jump:
 		# If you're on the floor or coyote time hasn't run out:
 		if is_on_floor() or coyote_time_timer < COYOTE_TIME:
 			coyote_time_timer = COYOTE_TIME + 1.0  # Disable coyote time
 			velocity.y = -jump_power  # Jump
+		
 	if not is_on_floor():
+		# Bonk against ceiling
 		if is_on_ceiling():
 			velocity.y = 0.0
 		
+		# Increment coyote time
 		if coyote_time_timer < COYOTE_TIME:
 			coyote_time_timer += delta
 			
