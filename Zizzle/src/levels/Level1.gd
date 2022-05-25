971dce25c4dfa9038c56c0b25ff150a54f7a14cc
winterbeak <winterbeak@gmail.com>
2022-05-25 00:14:15 -0400

    Add key to look down with

--- Zizzle/src/levels/Level1.gd
+++ Zizzle/src/levels/Level1.gd
@@ -7,7 +7,9 @@ onready var screen_size = get_viewport_rect().size
 
 var CAMERA_SPEED = 0.1
 # Camera is centered, so this places the player at 1/2 - 1/6 = 1/3 on the screen
-onready var CAMERA_DISTANCE_FROM_PLAYER = screen_size.y / 6
+onready var CAMERA_DEFAULT_PLAYER_DISTANCE = screen_size.y / 6
+# This places the player at 1/2 - 1/4 = 1/4 on the screen
+onready var CAMERA_DOWN_DISTANCE = screen_size.y / 4
 
 
 func _on_Player_collided_with_floor() -> void:
@@ -16,7 +18,14 @@ func _on_Player_collided_with_floor() -> void:
 
 
 func update_camera():
-	var target_y = player.position.y - CAMERA_DISTANCE_FROM_PLAYER
+	var down = Input.is_action_pressed("down")
+	
+	var target_y
+	if down:
+		target_y = player.position.y + CAMERA_DOWN_DISTANCE
+	else:
+		target_y = player.position.y - CAMERA_DEFAULT_PLAYER_DISTANCE
+	
 	camera.position.y = lerp(camera.position.y, target_y, CAMERA_SPEED)
 
 
