a823a254b34cb66dd9df7822ec872edccf5949ca
winterbeak <winterbeak@gmail.com>
2022-05-25 10:25:40 -0400

    Make camera framerate independant

--- Zizzle/src/levels/Level1.gd
+++ Zizzle/src/levels/Level1.gd
@@ -5,6 +5,7 @@ onready var player = $Player
 
 onready var screen_size = get_viewport_rect().size
 
+
 var CAMERA_SPEED = 0.1
 # Camera is centered, so this places the player at 1/2 - 1/6 = 1/3 on the screen
 onready var CAMERA_DEFAULT_PLAYER_DISTANCE = screen_size.y / 6
@@ -17,7 +18,7 @@ func _on_Player_collided_with_floor() -> void:
 	get_tree().change_scene("res://scenes/GameOver.tscn")
 
 
-func new_camera_pos(camera_pos: Vector2, player_pos: Vector2) -> Vector2:
+func new_camera_pos(dt: float, camera_pos: Vector2, player_pos: Vector2) -> Vector2:
 	var down = Input.is_action_pressed("down")
 	
 	var target_y
@@ -25,9 +26,10 @@ func new_camera_pos(camera_pos: Vector2, player_pos: Vector2) -> Vector2:
 		target_y = player_pos.y + CAMERA_DOWN_DISTANCE
 	else:
 		target_y = player_pos.y - CAMERA_DEFAULT_PLAYER_DISTANCE
-	
-	return Vector2(0.0, lerp(camera_pos.y, target_y, CAMERA_SPEED))
+
+	return Vector2(0.0, lerp(camera_pos.y, target_y, CAMERA_SPEED * dt * 60))
 
 
 func _process(dt):
-	camera.position = new_camera_pos(camera.position, player.position)
+	camera.position = new_camera_pos(dt, camera.position, player.position)
+ 
