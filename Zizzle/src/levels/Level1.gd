c8c63094ff2ff44327c3ea6bacb525fbd2e01d2a
winterbeak <winterbeak@gmail.com>
2022-05-25 00:05:01 -0400

    Add smooth uncentered camera

--- Zizzle/src/levels/Level1.gd
+++ Zizzle/src/levels/Level1.gd
@@ -1,7 +1,24 @@
 extends Node2D
 
+onready var camera = $Camera
+onready var player = $Player
+
+onready var screen_size = get_viewport_rect().size
+
+var CAMERA_SPEED = 0.1
+# Camera is centered, so this places the player at 1/2 - 1/6 = 1/3 on the screen
+onready var CAMERA_DISTANCE_FROM_PLAYER = screen_size.y / 6
 
 
 func _on_Player_collided_with_floor() -> void:
 	print("Game Over!")
 	get_tree().change_scene("res://scenes/GameOver.tscn")
+
+
+func update_camera():
+	var target_y = player.position.y - CAMERA_DISTANCE_FROM_PLAYER
+	camera.position.y = lerp(camera.position.y, target_y, CAMERA_SPEED)
+
+
+func _process(dt):
+	update_camera()
