a1a396d3bc77040469e112975ae51eac458893a0
winterbeak <winterbeak@gmail.com>
2022-05-26 18:04:13 -0400

    Make camera distances based off screen, not camera

--- Zizzle/src/levels/Level1.gd
+++ Zizzle/src/levels/Level1.gd
@@ -9,21 +9,10 @@ onready var screen_size = get_viewport_rect().size
 
 var CAMERA_SPEED = 0.1
 export var ROOF_DISPLAY_HEIGHT = 50  # How much of the roof to show
-onready var ROOF_CAMERA_OFFSET = camera_adjust_y(ROOF_DISPLAY_HEIGHT)
-onready var CAMERA_DEFAULT_PLAYER_DISTANCE = camera_adjust_y((2.0/3.0) * screen_size.y)
-onready var CAMERA_DOWN_DISTANCE = camera_adjust_y((3.0/4.0) * screen_size.y)
 
-
-# Changes a y-value relative to the top of the camera to a y-value in the world.
-# For example, passing 0 will return the y-value of the top of the camera in the world.
-func camera_adjust_y(y: float) -> float:
-	return camera_adjust(Vector2(0, y)).y
-
-
-# Changes a position relative to the top left of the camera to a position in the world.
-# For example, passing (0, 0) will return the position of the top left of the camera in the world.
-func camera_adjust(pos: Vector2) -> Vector2:
-	return pos - camera.position - screen_size / 2
+onready var ROOF_CAMERA_OFFSET = ROOF_DISPLAY_HEIGHT - screen_size.y / 2
+onready var CAMERA_DEFAULT_PLAYER_DISTANCE = (2.0/3.0) * screen_size.y - screen_size.y / 2
+onready var CAMERA_DOWN_DISTANCE = (3.0/4.0) * screen_size.y - screen_size.y / 2
 
 
 func _on_Player_collided_with_floor() -> void:
